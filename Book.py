5af1bc064281ae030ccb93c1186799548663a363
Nicholas Beers <nlbeers@gmail.com>
2021-11-29 22:39:35 -0600

    created Book class

---
+++ Book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, title, author, price, isbn):
+        self.title = title
+        self.author = author
+        self.price = price
+        self.isbn = isbn
+
+    
